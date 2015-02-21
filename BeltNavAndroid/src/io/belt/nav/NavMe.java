5f1a1ba4017026f04b8e41488561a2346dbaeee5
Caleb Dockter <calebdockter@gmail.com>
2015-02-21 13:57:08 -0800

    Creating base project for android components

---
+++ BeltNavAndroid/src/io/belt/nav/NavMe.java
@@ -0,0 +1,15 @@
+package io.belt.nav;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NavMe extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
