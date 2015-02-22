34b3d3053ae2402f78479a4fbe808d1761151a46
Caleb Dockter <calebdockter@gmail.com>
2015-02-22 13:55:16 -0800

    adding tou so we don't have negative radians

--- MotorControlPython/MotorSlot.py
+++ MotorControlPython/MotorSlot.py
@@ -1,4 +1,5 @@
 from Motor import Motor
+import math
 
 class MotorSlot(object):
 	def __init__(self, radiansFromAhead, motor):
@@ -6,12 +7,16 @@ class MotorSlot(object):
 		self.motor = motor
 	
 	def conditinalActivate(self, radiansPosition, radiansArch, durationInMs):
-		maxPosition = radiansPosition + (radiansArch/2)
-		minPosition = radiansPosition - (radiansArch/2)
+		tou = 2 * math.pi
+		maxPosition = (radiansPosition + (radiansArch/2)) + tou
+		minPosition = (radiansPosition - (radiansArch/2)) + tou
+
+		motorPosition = tou + self.radiansFromAhead
+
 		print "max: " + str(maxPosition)
 		print "min: " + str(minPosition)
 		print "current position: " + str(self.radiansFromAhead)
-		if(self.radiansFromAhead > minPosition and self.radiansFromAhead < maxPosition):
+		if(motorPosition > minPosition and motorPosition < maxPosition):
 			self.motor.start(durationInMs)
 	
 	def stop(self):
