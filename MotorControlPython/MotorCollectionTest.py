6fcb4df032c8791645fdc1b4b9d6ce9a0051a263
Caleb Dockter <calebdockter@gmail.com>
2015-02-22 14:32:09 -0800

    diag

--- MotorControlPython/MotorCollectionTest.py
+++ MotorControlPython/MotorCollectionTest.py
@@ -6,21 +6,27 @@ import math
 factory = MotorFactory()
 motorCollection = MotorCollection(factory.getMotors())
 
+print "-- all --"
 motorCollection.activate(0, 2*math.pi, 1000)
 time.sleep(2)
 
+print "-- left front --"
 motorCollection.activate(0, math.pi/2, 2000)
 time.sleep(3)
 
+print "-- left far --"
 motorCollection.activate(math.pi/4, 1, 2000)
 time.sleep(3)
 
+print "-- right front --"
 motorCollection.activate(math.pi/2, 1, 2000)
 time.sleep(3)
 
+print "-- left all --"
 motorCollection.activate(math.pi * 3/2, 1, 1000)
 time.sleep(2)
 
+print "-- left far --"
 motorCollection.activate(math.pi * 14/8, 1, 1000)
 time.sleep(2)
 
