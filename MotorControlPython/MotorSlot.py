c64c4f10d881979fdee7e87453bbbfac28aa1d66
Caleb Dockter <calebdockter@gmail.com>
2015-02-22 15:56:37 -0800

    halfway to normailzed

--- MotorControlPython/MotorSlot.py
+++ MotorControlPython/MotorSlot.py
@@ -7,24 +7,29 @@ class MotorSlot(object):
 		self.radiansFromAhead = self.normalize(radiansFromAhead)
 		self.motor = motor
 	
-	def conditinalActivate(self, radiansPosition, radiansArch, durationInMs):
+	def conditinalActivate(self, radiansDirection, radiansArch, durationInMs):
 		maxPosition = self.normalize(radiansPosition + (radiansArch/2))
 		minPosition = self.normalize(radiansPosition - (radiansArch/2))
 
-		motorPosition = self.radiansFromAhead
+		normailzedDirection = nomralize(radiansDirection)
+		offset = self.radiansFromAhead - normailzedDirection
 
-		print "max: " + str(maxPosition)
-		print "min: " + str(minPosition)
-		print "current position: " + str(motorPosition)
-		if(motorPosition > minPosition and motorPosition < maxPosition):
+		print "offect: " + str(offset)
+		print "direction: " + str(normailzedDirection)
+		print "motor position: " + str(self.radiansFromAhead)
+
+		if(offset < radiansArch):
 			self.motor.start(durationInMs)
+
+		#if(motorPosition > minPosition and motorPosition < maxPosition ):
+		#	self.motor.start(durationInMs)
 	
 	def stop(self):
 		self.motor.stop()
 
 	def normalize(self, radians):
-		if(radians > math.pi):
-			return math.pi - radians
-		if(radians < (-1 * math.pi)):
-			return radians + math.pi
+		if(radians > self.tou):
+			return normalize(radians - self.tou)
+		if(radians < 0):
+			return normalize(radians + self.tou)
 		return radians
