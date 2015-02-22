b1a488cfe69b5eee03fe0607fc9f95af0be9e154
Caleb Dockter <calebdockter@gmail.com>
2015-02-22 14:21:29 -0800

    Adding more test ligth sequences

--- MotorControlPython/MotorCollectionTest.py
+++ MotorControlPython/MotorCollectionTest.py
@@ -1,13 +1,29 @@
 from MotorFactory import MotorFactory
 from MotorCollection import MotorCollection
 import time
+import math
 
 factory = MotorFactory()
 motorCollection = MotorCollection(factory.getMotors())
 
-motorCollection.activate(0, 7, 1000)
+motorCollection.activate(0, 2*math.pi, 1000)
+time.sleep(2)
 
-time.sleep(6)
+motorCollection.activate(0, math.pi/2, 2000)
+time.sleep(3)
+
+motorCollection.activate(math.pi/4, 1, 2000)
+time.sleep(3)
+
+motorCollection.activate(math.pi/2, 1, 2000)
+time.sleep(3)
+
+motorCollection.activate(math.pi * 3/2, 1, 1000)
+time.sleep(2)
+
+motorCollection.activate(math.pi * 14/8, 1, 1000)
+time.sleep(2)
+
 print "cleaing up"
 factory.cleanup()
 exit()
