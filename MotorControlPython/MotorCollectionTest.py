cb064590bb5f62cf37cf952270c1605defffeb7d
Caleb Dockter <calebdockter@gmail.com>
2015-02-22 12:25:09 -0800

    Fixing test sleep method call

--- MotorControlPython/MotorCollectionTest.py
+++ MotorControlPython/MotorCollectionTest.py
@@ -1,10 +1,11 @@
 from MotorFactory import MotorFactory
 from MotorCollection import MotorCollection
+import time
 
 factory = MotorFactory()
 motorCollection = MotorCollection(factory.getMotors())
 
 motorCollection.activate(0, 6, 2)
 
-sleep(4)
+time.sleep(4)
 factory.cleanup()
