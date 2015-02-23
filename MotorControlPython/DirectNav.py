ee406779e4bbd37d715de765dda120b852321ca9
Caleb Dockter <calebdockter@gmail.com>
2015-02-22 16:52:23 -0800

    Adding WASD controls

---
+++ MotorControlPython/DirectNav.py
@@ -0,0 +1,26 @@
+from MotorFactory import MotorFactory
+from MotorCollection import MotorCollection
+import time
+import math
+
+
+factory = MotorFactory()
+motorCollection = MotorCollection(factory.getMotors())
+
+continue = True
+while continue:
+	command = raw_input("next: ")
+	if("q" == command):
+		continue = False
+
+	if("w" == command):
+		motorCollection.activate(0, math.pi/8, 1000)
+	if("a" == command):	
+		motorCollection.activate(math.pi/2, math.pi/8, 1000)
+	if("d" == command):	
+		motorCollection.activate(math.pi* 3/2, math.pi/8, 1000)
+	if("s" == command):	
+		motorCollection.activate(math.pi, math.pi * 2, 1000)
+
+factory.cleanup()
+exit()
