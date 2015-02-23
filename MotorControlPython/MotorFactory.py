4a495d2fac3be48069bd1c35ae7bb3565a57c1e5
Caleb Dockter <calebdockter@gmail.com>
2015-02-22 16:22:44 -0800

    Setting motor positions

--- MotorControlPython/MotorFactory.py
+++ MotorControlPython/MotorFactory.py
@@ -15,10 +15,10 @@ class MotorFactory(object):
 
 	def getMotors(self):
 		motors = []
-		motors.append(MotorSlot(math.pi/8, Motor(17, self.gpio)))
+		motors.append(MotorSlot(math.pi/8, Motor(23, self.gpio)))
 		motors.append(MotorSlot(math.pi * 15/8, Motor(27, self.gpio)))
-		motors.append(MotorSlot(math.pi * 1/4, Motor(22, self.gpio)))
-		motors.append(MotorSlot(math.pi * 3/2, Motor(23, self.gpio)))
+		motors.append(MotorSlot(math.pi * 1/4, Motor(17, self.gpio)))
+		motors.append(MotorSlot(math.pi * 3/2, Motor(22, self.gpio)))
 		# motors.append(Motor(24, self.gpio))
 		# motors.append(Motor(25, self.gpio))
 		return motors
